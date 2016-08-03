3160b2e15dcc7fbfaa37fdeb17f8eb0fb70b4343
Jack McGurran <jack.mcgurran@warnermusic.com>
2016-08-03 14:27:58 +0100

    added index.php

---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<title></title>
+<link rel="icon" type="image/png" href="images/favicon.png" />
+
+<meta http-equiv="Content-Type" content="text/html;charset=utf-8" />
+<meta property="og:url" content="" />
+<meta property="og:title" content="" />
+<meta name="description" content="" />
+<meta property="og:image" content="" />
+<meta property="og:site_name" content="" />
+
+<link rel="stylesheet" href="css/main.css">
+
+<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+
+<script type="text/javascript">
+$(document).ready(function(){
+
+});
+</script>
+
+</head>
+<body>
+
+</body>
+</html>
